e0b2dca53ba2670a96a7ac379b572d6b7c79732b
Galen Lalor <bb.42001@gmail.com>
2023-04-11 14:42:14 +0930

    new files

---
+++ models/User.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    phoneNumber: {
+      type: Number,
+      min: 8,
+    },
+    animalAge: {
+      type: Number,
+      max: 50,
+    },
+    animalName: String,
+    animalBreed: String,
+    animalGender: String,
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
